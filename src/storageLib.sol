ad5746f8b6ff58880622a804c687bcb950425d1b
youssefea <youcefea99@gmail.com>
2024-07-12 23:42:23 +0200

    add commits

---
+++ src/storageLib.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {BasicParticle, SemanticMoney, FlowRate, Value, Time} from "@superfluid-finance/solidity-semantic-money/src/SemanticMoney.sol";
+
+struct User {
+    address before;
+    address afte;
+    int96 flowrate;
+}
+
+struct Storage {
+    mapping(address => User) list;
+    address head;
+}
+
+//type Storage is Data;
+
+library StorageLib {
+    /**
+     * @notice a 
+     * @param data The users struct.
+     * @param p The player.
+     * @param f The flowrate.
+     */
+    function addPlayer(Storage storage data, address p, int96 f) internal {
+        // check if there is a head. If there isn't, this is the first user        
+        for (address i = data.head; i != address(0); i = data.list[i].afte) {
+            if (data.list[i].flowrate < f) {
+                data.list[p].afte = i;
+                data.list[p].before = data.list[i].before;
+                data.list[i].before = p;
+                data.list[p].flowrate = f;
+                return;
+            }
+        }
+        if(data.head == address(0)){
+            data.head = p; 
+            data.list[p].flowrate = f;
+        } 
+
+    }
+
+    function updatePlayer(Storage storage data, address p, int96 f) internal {
+        removePlayer(data, p);
+        addPlayer(data, p, f);
+    }
+
+    function removePlayer(Storage storage data, address p) internal {
+        data.list[data.list[p].before].afte = data.list[p].afte;
+        data.list[data.list[p].afte].before = data.list[p].before;
+        if (data.head == p) data.head = data.list[p].afte;
+        delete data.list[p];
+    }
+
+    // TODO: add player with position
+}
+
+//using LibScaler for Scaler global;
